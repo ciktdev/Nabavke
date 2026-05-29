9f1249bc5517aa068019e3b6692725fdd152c473
ciktdev <ciktdev@gmail.com>
2026-05-29 11:03:17 +0200

    Skrivanje sifri

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql2');
 
+// 💡 Učitavamo dotenv konfiguraciju (ovo omogućava čitanje .env fajla)
+require('dotenv').config();
 
-// Konekcija sa bazom
+// Konekcija sa bazom pomoću varijabli iz okruženja
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',      // tvoj username
-    password: 'moja_sifra123',      // tvoja lozinka
-    database: 'nabavke'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, // 💡 Lozinka se sada bezbedno vuče odavde
+    database: process.env.DB_DATABASE
 });
 
 db.connect((err) => {
     if (err) throw err;
-    console.log('Povezan na MySQL bazu!');
+    console.log('Povezan na MySQL bazu preko eksternih parametara!');
 });
 
 module.exports = db;
